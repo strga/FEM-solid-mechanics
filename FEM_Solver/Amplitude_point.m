b033288d4637f124ba884f70620d6246119b695b
strga <151577860+strga@users.noreply.github.com>
2026-09-10 04:54:29 +0200

    Update Amplitude_point.m

--- FEM_Solver/Amplitude_point.m
+++ FEM_Solver/Amplitude_point.m
@@ -3,8 +3,7 @@ clc;
 close all;
 
 % Load the displacement data from file
-% data = load('/Users/strihavka/Documents/FEM_Diplom/FEM/Vibration_data/Bending_5/New_Linear_point_displacement_5.dat'); %...1-3
-data = load('/Users/strihavka/Documents/FEM_Diplom/FEM/Vibration_data/Linear_point_displacement_3D_6.dat'); %...1-3
+data = load('Linear_displacement_3D.dat');
 time = data(:, 1);
 amplitude = data(:, 2);
 
